05efbca837c550cc194748635a537104d693135e
Mike <mike@mike-francis.org>
2025-09-03 22:45:15 +0100

    header is a work in progress. need to finish buttons and connect up logic

---
+++ src/components/Corners/Corners.tsx
@@ -0,0 +1,12 @@
+import styles from "./Corners.module.css";
+
+const Corners = () => {
+  return (
+    <div className={styles.corners}>
+      <div className={styles.tr}></div>
+      <div className={styles.tl}></div>
+    </div>
+  );
+};
+
+export default Corners;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.column_left}>
+        <h1 className={styles.h1}>Repository Dashboard</h1>
+        <h2 className={styles.h2}>Monitor dependencies, security and project health</h2>
+        <p className={styles.p}>Connect to: &lt;insert dynamic name here&gt;</p>
+      </div>
+      <div className={styles.column_right}>
+        <button>test</button>
+        <button>test</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
