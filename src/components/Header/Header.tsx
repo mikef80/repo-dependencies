cbda72ac648f554e0cbd4edccff192b06510b986
Mike <mike@mike-francis.org>
2025-09-17 13:49:53 +0100

    removed secret

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,9 @@ import { RefreshCw, Settings } from "lucide-react";
 import styles from "./Header.module.css";
 import GitHubModal from "../GitHubModal/GitHubModal";
 import { useState } from "react";
-import type { gitHubDetailsType } from "../../types/types";
+import type { gitHubDetailsType, Repo } from "../../types/types";
+
+import { fetchLanguageDetails, fetchRepoData } from "../../utils/repoUtils";
 
 const Header = () => {
   const [showGitHubModal, setShowGitHubModal] = useState(false);
@@ -10,9 +12,21 @@ const Header = () => {
     username: "",
     token: "",
   });
+  const [repos, setRepos] = useState<Repo[]>([]);
 
   const toggleGitHubModal = () => {
     setShowGitHubModal((currentShowState) => !currentShowState);
+  };
+
+  const onRefreshClick = async () => {
+    const fetchedRepos = await fetchRepoData(gitHubDetails);
+
+    fetchedRepos.forEach(async (repo) => {
+      const { data } = await fetchLanguageDetails(gitHubDetails, repo);
+      repo.languages = data;
+    });
+
+    setRepos(fetchedRepos);
   };
 
   return (
@@ -21,17 +35,18 @@ const Header = () => {
         <div className={styles.column_left}>
           <h1 className={styles.h1}>Repository Dashboard</h1>
           <h2 className={styles.h2}>Monitor dependencies, security and project health</h2>
-          <p className={styles.p}>Connected to: &lt;insert dynamic name here&gt;</p>
+          <p className={styles.p}>Connected to: @{gitHubDetails.username}</p>
         </div>
         <div className={styles.column_right}>
           <button className={styles.button_settings} onClick={toggleGitHubModal}>
             <Settings className={styles.settings} />
           </button>
-          <button className={styles.button_refresh}>
+          <button className={styles.button_refresh} onClick={onRefreshClick}>
             <RefreshCw className={styles.refresh} />
             Refresh
           </button>
         </div>
+        private: {repos.filter((repo) => repo.private === false).length}
       </header>
 
       {showGitHubModal && (
---
+++ src/utils/repoUtils.ts
@@ -0,0 +1,73 @@
+import type { Repo, gitHubDetailsType } from "../types/types";
+import axios from "axios";
+
+export const transformRepo = (repo: any): Repo => ({
+  dependencies: 0, // placeholder
+  dependencyDetails: [],
+  dependencyStatus: "current", // placeholder
+  description: repo.description,
+  forks: repo.forks_count,
+  htmlUrl: repo.html_url,
+  id: repo.id,
+  languages: {},
+  languages_url: repo.languages_url,
+  lastUpdate: repo.updated_at,
+  name: repo.name,
+  private: repo.private,
+  size: repo.size,
+  stars: repo.stargazers_count,
+  topics: repo.topics ?? [],
+  vulnerabilities: 0, // placeholder
+});
+
+export const fetchRepoData = async (gitHubDetails: gitHubDetailsType) => {
+  let url = "";
+  if (!gitHubDetails.token) {
+    url = `https://api.github.com/users/${gitHubDetails.username}/repos`;
+  } else {
+    url = `https://api.github.com/user/repos`;
+  }
+
+  try {
+    const { data } = await axios.get(url, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        ...(gitHubDetails.token && { Authorization: `Bearer ${gitHubDetails.token}` }),
+      },
+      params: {
+        sort: "updated",
+        per_page: 100,
+        type: "owner",
+      },
+    });
+
+    const structuredRepos: Repo[] = data.map((repo: any) => transformRepo(repo));
+
+    return structuredRepos;
+  } catch (error) {
+    console.error("Error fetching repositories:", error);
+    throw new Error("Failed to fetch repositories");
+  }
+};
+
+export const fetchLanguageDetails = async (gitHubDetails: gitHubDetailsType, repo: Repo) => {
+  const url = repo.languages_url;
+
+  if (!url) {
+    throw new Error("languages_url is null");
+  }
+
+  try {
+    const data = await axios.get(url, {
+      headers: {
+        Accept: "application/vnd.github.v3+json",
+        ...(gitHubDetails.token && { Authorization: `Bearer ${gitHubDetails.token}` }),
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching languages:", error);
+    throw new Error("Failed to fetch languages");
+  }
+};
