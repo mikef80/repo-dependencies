0528a70fdd875df222b3d813183311a03bd41c8e
Mike <mike@mike-francis.org>
2025-10-09 21:05:46 +0100

    correct number of dependencies and devDependencies now being returned. fixes #14"

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,11 +14,12 @@ export type Repo = {
   lastUpdate: string; // ISO date string from API, format later in UI
   dependencyStatus: string; // custom, not from API
   vulnerabilities: number; // custom, not from API
-  dependencies: number; // custom, not from API
+  dependencies: {}; // custom, not from API
+  devDependencies: {};
   size: number; // number in KB from API, format to "51.0 KB"
   htmlUrl: string;
   topics: string[];
-  dependencyDetails: any[]; // custom, define shape later if needed
+  // dependencyDetails: any[]; // custom, define shape later if needed
   private: boolean;
 };
 
